172bccd5102da42d3d193d1827cedb281225a5e6
ARIJIT14KONAR <konararijit14@gmail.com>
2024-08-23 10:28:32 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings: ["Front-end Developer", "Programmer", "Photographer ", "Athlete"],
+    typeSpeed: 75,
+    backSpeed: 75,
+    backDelay: 1000,
+    loop: true
+});
